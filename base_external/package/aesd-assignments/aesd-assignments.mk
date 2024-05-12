14ff93417ee948382d441ba68a88e8af810ff459
Julian Müllner <julian-muellner@outlook.com>
2024-05-12 18:33:33 +0200

    updated version of aesd

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'c922de26e1b12f9112dabdc51721cbef51c49e83'
+AESD_ASSIGNMENTS_VERSION = 'e0614e8'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
